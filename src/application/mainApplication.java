64fe42f7c92de1e5334466d50e22c9580b3151da
AlexandreVaz <amvaz30@gmail.com>
2011-11-27 23:45:47 -0200

    teste

--- src/application/mainApplication.java
+++ src/application/mainApplication.java
@@ -6,8 +6,7 @@ public class mainApplication {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("mainApplication.main()");
 	}
 
 }
